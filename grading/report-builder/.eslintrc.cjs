c80b20204f06949389a03e843b10dec0622702b6
Andrew C. Oliver <acoliver@gmail.com>
2025-10-19 03:32:24 -0300

    Add report builder eval harness

---
+++ grading/report-builder/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  env: {
+    node: true,
+    es2021: true
+  },
+  ignorePatterns: []
+};
---
+++ problems/report-builder/workspace/vitest.config.public.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['tests/public/**/*.spec.ts'],
+    environment: 'node',
+    globals: true
+  }
+});
